11085f3514c79f168c722fb2d654a4a32708df22
Rony <Rony@Alex--Nicoles-MacBook-Pro.local>
2016-05-20 11:50:51 -0700

    first commit

---
+++ themes/inhabitent/page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<p> Hello world </p>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
