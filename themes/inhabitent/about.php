1ff07acb52884d20c905c66e1cadb69ce813f2a3
Rony <Rony@Alex--Nicoles-MacBook-Pro.local>
2016-06-07 11:07:53 -0700

    styles about page

--- themes/inhabitent/about.php
+++ themes/inhabitent/about.php
@@ -9,12 +9,15 @@ get_header(); ?>
 
   <div id="primary" class="about-area">
       <main id="main" class="site-main" role="main">
+
         <div class="about-content">
+
           <section class="about-header">
               <h1>About</h1>
           </section>
 
           <section class="about container">
+
               <div class=" about-paragraphs">
                   <h2 class="about-title">Our Story</h2>
                       <?php echo CFS()->get( 'our_story' ); ?>
@@ -22,7 +25,7 @@ get_header(); ?>
                       <?php echo CFS()->get( 'our_team' ); ?>
               </div>
           </section>
-        </div>
+        </div> <!-- .ABOUT-CONTENT -->
       </main><!-- #main -->
   </div><!-- #primary -->
 <?php get_footer(); ?>
